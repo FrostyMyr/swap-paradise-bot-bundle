0e0b14811de117ab68a44acae12c59e161c11413
FrostyMyr <123850469+FrostyMyr@users.noreply.github.com>
2024-04-16 10:40:50 +0700

    Create guildMemberUpdate.js

---
+++ profile-shifter/events/guildMemberUpdate.js
@@ -0,0 +1,29 @@
+const { ChannelType, PermissionsBitField } = require("discord.js");
+const proxy = require("../proxy.js");
+
+module.exports = (client) => {
+  client.on("guildMemberUpdate", async (oldMember, newMember) => {
+    const verifiedRole = newMember.guild.roles.cache.find(
+      (role) => role.name === "verified",
+    );
+
+    if (!verifiedRole) {
+      console.log("Role 'verified' not found.");
+      return;
+    }
+
+    const hadVerifiedRole = oldMember.roles.cache.has(verifiedRole.id);
+    const hasVerifiedRole = newMember.roles.cache.has(verifiedRole.id);
+
+    if (!hadVerifiedRole && hasVerifiedRole) {
+      if (!newMember.user.avatar) {
+        await newMember.roles.remove(verifiedRole);
+        console.log(`Removed 'verified' role from ${newMember.user.tag} due to no avatar.`);
+      } else {
+        proxy.autoProfileShiftInteraction(client, ChannelType, PermissionsBitField);
+      }
+    } else if (hadVerifiedRole && !hasVerifiedRole) {
+      proxy.autoProfileShiftInteraction(client, ChannelType, PermissionsBitField);
+    }
+  });
+};
